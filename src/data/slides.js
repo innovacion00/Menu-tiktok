d36ba41e24dcee88a0960e50eb48e6f2fc6f7ffc
SebasKB <sekelbi99@hotmai.com>
2026-03-12 10:37:32 -0500

    scroll infinito

--- src/data/slides.js
+++ src/data/slides.js
@@ -4,25 +4,35 @@
  */
 export const slides = [
   {
+    id: 0,
+    image: "https://space-img.sfo3.digitaloceanspaces.com/test/ChatGPT%20Image%2012%20mar%202026,%2008_49_48%20a.m..png",
+    title: "Bienvenido al Hotel Aixo",
+    description: "Conoce nuestro nuevo menu digital!",
+    price: "Desliza hacia abajo para ver nuestros platos",
+  },
+  {
     id: 1,
-    image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=800",
-    title: "Ensalada César",
-    description: "Lechuga romana, crutones, parmesano y aderezo césar cremoso.",
-    price: "€9.50",
+    image: "https://mir-s3-cdn-cf.behance.net/project_modules/source/37e19c53319193.592ffe63763e0.gif",
+    title: "Hamburguesa chascoberto",
+    description: "Lechuga romana, tomate himalayesco, parmesano y aderezo chascoberto cremoso.",
+    price: "29.500$ COP",
+    tags: ["queso", "carne", "signature"],
   },
   {
     id: 2,
-    image: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=800",
+    image: "https://i.pinimg.com/originals/98/2d/78/982d78c54b0b581c36309d76e509e751.gif",
     title: "Pizza Margherita",
-    description: "Masa artesana, tomate San Marzano, mozzarella y albahaca fresca.",
-    price: "€12.00",
+    description: "Masa artesana, tomate San Marzano, mozzarella y peperiooni.",
+    price: "50.000$ COP",
+    tags: ["queso", "peperoni"],
   },
   {
     id: 3,
-    image: "https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?w=800",
-    title: "Tostadas con aguacate",
-    description: "Pan de masa madre, aguacate, huevo poché y semillas.",
-    price: "€8.00",
+    image: "https://media.tenor.com/y3nqK0Wu3HAAAAAM/pouring-syrup-on-waffles-matty-matheson.gif",
+    title: "Waffles con sirope de arce",
+    description: "waffles con sirope de arce y frutos rojos.",
+    price: "15.000$ COP",
+    tags: ["dulce", "desayuno"],
   },
   {
     id: 4,
@@ -30,6 +40,7 @@ export const slides = [
     title: "Smoothie de frutos rojos",
     description: "Fresas, arándanos, plátano y leche de avena.",
     price: "€5.50",
+    tags: ["vegano", "frutas"],
   },
   {
     id: 5,
@@ -37,6 +48,7 @@ export const slides = [
     title: "Hamburguesa clásica",
     description: "Carne de ternera, lechuga, tomate, cebolla y salsa especial.",
     price: "€11.00",
+    tags: ["queso", "clásico"],
   },
   {
     id: 6,
@@ -44,5 +56,6 @@ export const slides = [
     title: "Bowl de poke",
     description: "Atún fresco, arroz, aguacate, edamame y salsa de soja.",
     price: "€13.50",
+    tags: ["fresco", "mar"],
   },
 ];
